022b818004f7f177df82fe517938684e0d31fe37
Aryainguz <aryaninguz369@gmail.com>
2024-03-29 23:24:50 +0530

    feat: made home page nav responsive

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,25 +1,41 @@
+"use client";
 import { SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
+import logo from "../public/assets/image.png";
+import Image from "next/image";
+import Link from "next/link";
 
 const Home = () => {
+  const showMobileNav = () => {
+    document.querySelector(".menu")?.classList.toggle("hidden");
+  };
   return (
     <>
       <div className="overflow-x-hidden bg-gray-50">
         <header className="py-4 md:py-6">
           <div className="container px-4 mx-auto sm:px-6 lg:px-8">
             <div className="flex items-center justify-between">
-              <div className="flex-shrink-0">
+              <div className="flex-shrink-0 p-4 sm:p-0">
                 <a
                   href="#"
-                  className="flex rounded outline-none focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+                  className="flex rounded outline-none font-bold text-3xl focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
                 >
-                  <img
-                    className="w-auto h-8"
-                    src="https://cdn.rareblocks.xyz/collection/clarity/images/logo.svg"
+                  <Image
+                    src={logo}
+                    width={35}
+                    height={30}
+                    className="mx-1"
+                    alt=""
                   />
+                  Pic
+                  <span className="text-blue-600">Wise</span>.
                 </a>
               </div>
               <div className="flex lg:hidden">
-                <button type="button" className="text-gray-900">
+                <button
+                  type="button"
+                  className="text-gray-900"
+                  onClick={showMobileNav}
+                >
                   <span x-show="!expanded" aria-hidden="true">
                     <svg
                       className="w-7 h-7"
@@ -33,22 +49,6 @@ const Home = () => {
                         strokeLinejoin="round"
                         strokeWidth="1.5"
                         d="M4 6h16M4 12h16M4 18h16"
-                      />
-                    </svg>
-                  </span>
-                  <span x-show="expanded" aria-hidden="true">
-                    <svg
-                      className="w-7 h-7"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M6 18L18 6M6 6l12 12"
                       />
                     </svg>
                   </span>
@@ -79,41 +79,63 @@ const Home = () => {
                 </a>
               </div>
               <div className="hidden lg:ml-auto lg:flex lg:items-center lg:space-x-10">
-
-
-
-                <UserButton afterSignOutUrl="/" />
-
-                {/* below part handles sign in button and only renders when user is not signed out  */}
-                <SignedOut>
-                  <a
-                    href="#"
-                    className="inline-flex items-center justify-center px-6 py-3 text-base font-bold leading-7 text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-xl hover:bg-gray-600 font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-                    role="button"
-                  >
-                    <SignInButton afterSignInUrl="/dashboard" mode="modal" />
-                  </a>
-                </SignedOut>
-
-                
-
-
-
+                <Link href="/dashboard">
+                  <SignInButton afterSignInUrl="/dashboard" mode="modal">
+                    <div
+                      className="inline-flex items-center justify-center px-6 py-3 text-base font-bold leading-7 text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-xl hover:bg-blue-700 font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-900"
+                      role="button"
+                    >
+                      Dashboard
+                    </div>
+                  </SignInButton>
+                </Link>
               </div>
             </div>
-            {/* <nav x-show="expanded" x-collapse>
-        <div className="px-1 py-8">
-          <div className="grid gap-y-7">
-            <a href="#" className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50 focus:outline-none font-pj focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Features </a>
-            <a href="#" className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50 focus:outline-none font-pj focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Pricing </a>
-            <a href="#" className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50 focus:outline-none font-pj focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Automation </a>
-            <a href="#" className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50 focus:outline-none font-pj focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Customer Login </a>
-            <a href="#" className="inline-flex items-center justify-center px-6 py-3 text-base font-bold leading-7 text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-xl hover:bg-gray-600 font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900" role="button">
-              Sign up
-            </a>
-          </div>
-        </div>
-      </nav> */}
+            <nav className="hidden menu">
+              <div className="px-1 py-8">
+                <div className="grid gap-y-7">
+                  <a
+                    href="#"
+                    className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50 focus:outline-none font-pj focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+                  >
+                    {" "}
+                    Features{" "}
+                  </a>
+                  <a
+                    href="#"
+                    className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50 focus:outline-none font-pj focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+                  >
+                    {" "}
+                    Pricing{" "}
+                  </a>
+                  <a
+                    href="#"
+                    className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50 focus:outline-none font-pj focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+                  >
+                    {" "}
+                    Automation{" "}
+                  </a>
+                  <a
+                    href="#"
+                    className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition-all duration-200 rounded-xl hover:bg-gray-50 focus:outline-none font-pj focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+                  >
+                    {" "}
+                    Customer Login{" "}
+                  </a>
+                  <Link href="/dashboard">
+                    <SignInButton afterSignInUrl="/dashboard" mode="modal">
+                      
+                      <div
+                        className="inline-flex items-center justify-center px-6 py-3 text-base font-bold leading-7 text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-xl hover:bg-blue-700 font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-900"
+                        role="button"
+                      >
+                        Dashboard
+                      </div>
+                    </SignInButton>
+                  </Link>
+                </div>
+              </div>
+            </nav>
           </div>
         </header>
 
@@ -121,26 +143,26 @@ const Home = () => {
           <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="max-w-2xl mx-auto text-center">
               <h1 className="px-6 text-lg text-gray-600 font-inter">
-                Smart email campaign builder, made for Developers
+                Redefining Image Compression, Like Never Before.
               </h1>
-              <p className="mt-5 text-4xl font-bold leading-tight text-gray-900 sm:leading-tight sm:text-5xl lg:text-6xl lg:leading-tight font-pj">
-                Turn your visitors into profitable
+              <p className="mt-5 text-4xl font-bold leading-tight text-gray-900 sm:leading-tight sm:text-5xl lg:text-5xl lg:leading-tight font-pj">
+                Compress image size by 90% wihout
                 <span className="relative inline-flex sm:inline">
-                  <span className="bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] blur-lg filter opacity-30 w-full h-full absolute inset-0" />
-                  <span className="relative"> business </span>
+                  <span className="bg-gradient-to-r from-blue-500 to-blue-600 blur-lg filter opacity-30 w-full h-full absolute inset-0" />
+                  <span className="relative">losing much of pixels.</span>
                 </span>
               </p>
               <div className="px-8 sm:items-center sm:justify-center sm:px-0 sm:space-x-5 sm:flex mt-9">
-                <a
-                  href="#"
-                  className="inline-flex items-center justify-center w-full px-8 py-3 text-lg font-bold text-white transition-all duration-200 bg-gray-900 border-2 border-transparent sm:w-auto rounded-xl font-pj hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                <Link
+                  href="/dashboard"
+                  className="inline-flex items-center justify-center w-full px-8 py-3 text-lg font-bold text-white transition-all duration-200 bg-blue-600 border-2 border-transparent sm:w-auto rounded-xl font-pj hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-900"
                   role="button"
                 >
-                  Get more customers
-                </a>
+                  Get Started
+                </Link>
                 <a
                   href="#"
-                  className="inline-flex items-center justify-center w-full px-6 py-3 mt-4 text-lg font-bold text-gray-900 transition-all duration-200 border-2 border-gray-400 sm:w-auto sm:mt-0 rounded-xl font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 hover:bg-gray-900 focus:bg-gray-900 hover:text-white focus:text-white hover:border-gray-900 focus:border-gray-900"
+                  className="inline-flex items-center justify-center w-full px-6 py-3 mt-4 text-lg font-bold text-gray-900 transition-all duration-200 border-2 border-gray-400 sm:w-auto sm:mt-0 rounded-xl font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 hover:bg-gray-200 focus:bg-gray-200"
                   role="button"
                 >
                   <svg
@@ -158,11 +180,11 @@ const Home = () => {
                       strokeLinejoin="round"
                     />
                   </svg>
-                  Watch free demo
+                  Watch the demo
                 </a>
               </div>
               <p className="mt-8 text-base text-gray-500 font-inter">
-                60 Days free trial · No credit card required
+                Free Image Compression.
               </p>
             </div>
           </div>
@@ -171,10 +193,10 @@ const Home = () => {
               <div className="absolute inset-0 h-2/3 bg-gray-50" />
               <div className="relative mx-auto">
                 <div className="lg:max-w-6xl lg:mx-auto">
-                  <img
+                  {/* <img
                     className="transform scale-110"
                     src="https://cdn.rareblocks.xyz/collection/clarity/images/hero/2/illustration.png"
-                  />
+                  /> */}
                 </div>
               </div>
             </div>
