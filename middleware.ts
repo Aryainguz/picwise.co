8e48961705dbd55bb7190c42bb42b6ca35ecedc4
Aryainguz <aryaninguz369@gmail.com>
2024-03-29 23:26:50 +0530

    feat: added clerk auth

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { authMiddleware } from "@clerk/nextjs";
+ 
+
+export default authMiddleware({
+    publicRoutes:["/"]
+});
+ 
+export const config = {
+  matcher: [
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/(api|trpc)(.*)"
+  ]
+};
