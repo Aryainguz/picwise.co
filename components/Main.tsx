3cb421c2f7473cdc49fcd5b5b0df6a92540bf216
Aryainguz <aryaninguz369@gmail.com>
2024-09-08 18:05:55 +0530

    fix: fixed mobile dropbox bug for converter

--- components/Main.tsx
+++ components/Main.tsx
@@ -29,11 +29,21 @@ export default function Main() {
   const [width, setWidth] = useState<number>(600);
   const [height, setHeight] = useState<number>(400);
   const [format, setFormat] = useState<string>("png");
+  const [loading, setloading] = useState<boolean>(false);
 
   const removeFile = () => {
     setFile(null);
     setPrev(null);
   };
+
+
+    // for mobile
+    const onFileSelect = (file: any) => {
+      setloading(true);
+      setFile(file);
+      setFileType(file.type);
+      setloading(false);
+    };
 
   const handleImageProcessing = async () => {
     if (file) {
@@ -139,37 +149,85 @@ export default function Main() {
     <main className="flex flex-col gap-4 sm:px-6 sm:py-0 md:gap-8">
       <div className="flex flex-col gap-4 md:grid md:grid-cols-1 lg:grid-cols-[1fr_300px]">
         {/* Dropbox Section */}
-        <div className="flex flex-col items-center justify-center bg-muted/40 rounded-lg p-2 sm:p-8">
-          <div
-            className="my-6 h-full w-full flex-col items-center justify-center rounded-lg border-2 border-dashed border-blue-500 bg-blue-50 text-blue-500 transition-colors hover:border-blue-600 dark:border-blue-700 dark:bg-blue-900 dark:text-blue-400 dark:hover:border-blue-600 dark:hover:bg-blue-800 flex"
-            {...getRootProps()}
-          >
-            <label
-              htmlFor="dropzone-file"
-              className="flex flex-col items-center justify-center w-full h-64 rounded-lg cursor-pointer bg-blue-50"
-            >
-              <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                <svg
-                  className="w-8 h-8 mb-4 text-blue-500"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 16"
+        <div className="flex flex-col items-center justify-center gap-8">
+            <div className="flex w-full max-w-4xl flex-col items-center justify-center rounded-lg border border-blue-200 bg-white p-8 shadow-lg dark:border-blue-800 dark:bg-blue-900 ">
+              <h1 className="text-3xl font-bold text-blue-950 dark:text-white">
+              Drag and Drop Images
+              </h1>
+              <p className="mt-2 text-gray-500 dark:text-gray-400">
+                Upload your image to get started.
+              </p>
+
+              <div
+                className="mt-6 h-64 w-full flex-col items-center justify-center rounded-lg border-2 border-dashed border-blue-500 bg-blue-50 p-4 text-blue-500 transition-colors hover:border-blue-600  dark:border-blue-700 dark:bg-blue-900 dark:text-blue-400 dark:hover:border-blue-600 dark:hover:bg-blue-800 hidden xl:flex"
+                {...getRootProps()}
+              >
+                <label
+                  htmlFor="dropzone-file"
+                  className="flex flex-col items-center justify-center w-full h-64 rounded-lg cursor-pointer bg-blue-50"
                 >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                    <svg
+                      className="w-8 h-8 mb-4 text-blue-500"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 16"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                      />
+                    </svg>
+                    {isDragActive ? (
+                      <p className="mb-2 p-4 sm:p-0 font-bold text-xl text-gray-500">
+                        <span>Drop the files here</span>
+                      </p>
+                    ) : (
+                      <>
+                        <p className="mb-2 p-4 sm:p-0 font-bold text-xl text-gray-500">
+                          <span>Click to upload</span> or{" "}
+                          <span className="text-blue-500">Drag</span> and
+                          <span className="text-blue-500"> Drop</span>
+                        </p>
+                        <p className="text-xs text-gray-500">
+                          SVG, PNG, JPG or GIF
+                        </p>
+                      </>
+                    )}
+                  </div>
+                  <input
+                    id="dropzone-file"
+                    className="hidden"
+                    {...getInputProps()}
                   />
-                </svg>
-                {isDragActive ? (
-                  <p className="mb-2 p-4 sm:p-0 font-bold text-xl text-gray-500">
-                    <span>Drop the files here</span>
-                  </p>
-                ) : (
-                  <>
+                </label>
+              </div>
+
+              <div className="flex xl:hidden mt-6 h-64 w-full flex-col items-center justify-center rounded-lg border-2 border-dashed border-blue-500 bg-blue-50 p-4 text-blue-500 transition-colors hover:border-blue-600  dark:border-blue-700 dark:bg-blue-900 dark:text-blue-400 dark:hover:border-blue-600 dark:hover:bg-blue-800">
+                <label
+                  htmlFor="dropzone-file"
+                  className="flex flex-col items-center justify-center w-full h-64 rounded-lg cursor-pointer bg-blue-50"
+                >
+                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                    <svg
+                      className="w-8 h-8 mb-4 text-blue-500"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 16"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                      />
+                    </svg>
                     <p className="mb-2 p-4 sm:p-0 font-bold text-xl text-gray-500">
                       <span>Click to upload</span> or{" "}
                       <span className="text-blue-500">Drag</span> and
@@ -178,16 +236,19 @@ export default function Main() {
                     <p className="text-xs text-gray-500">
                       SVG, PNG, JPG or GIF
                     </p>
-                  </>
-                )}
+                  </div>
+                  <input
+                    id="dropzone-file"
+                    type="file"
+                    className="hidden"
+                    onChange={(e: React.ChangeEvent<any>) =>
+                      onFileSelect(e.target.files[0])
+                    }
+                  />
+                </label>
               </div>
-              <input
-                id="dropzone-file"
-                className="hidden"
-                {...getInputProps()}
-              />
-            </label>
-          </div>
+            </div>
+
           {file && (
             <FilePreview file={file} removeFile={removeFile} prev={prev} />
           )}
