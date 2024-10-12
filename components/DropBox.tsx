29aa5150adf9394e6eb94640d89193aeb11ebbca
Ahsan khalid <ahsankhalid816@gmail.com>
2024-10-13 00:24:15 +0500

    Removed duplicate behaviour of the upload drag & drop folder opening and updated the code with optimization.

--- components/DropBox.tsx
+++ components/DropBox.tsx
@@ -85,57 +85,48 @@ const DropBox = () => {
               </p>
 
               <div
-                className="mt-6 h-64 w-full flex-col items-center justify-center rounded-lg border-2 border-dashed border-blue-500 bg-blue-50 p-4 text-blue-500 transition-colors hover:border-blue-600  dark:border-blue-700 dark:bg-blue-900 dark:text-blue-400 dark:hover:border-blue-600 dark:hover:bg-blue-800 hidden xl:flex"
+                className="mt-6 h-64 w-full flex-col items-center justify-center rounded-lg border-2 border-dashed border-blue-500 bg-blue-50 p-4 text-blue-500 transition-colors hover:border-blue-600 dark:border-blue-700 dark:bg-blue-900 dark:text-blue-400 dark:hover:border-blue-600 dark:hover:bg-blue-800 hidden xl:flex cursor-pointer"
                 {...getRootProps()}
               >
-                <label
-                  htmlFor="dropzone-file"
-                  className="flex flex-col items-center justify-center w-full h-64 rounded-lg cursor-pointer bg-blue-50"
-                >
-                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                    <svg
-                      className="w-8 h-8 mb-4 text-blue-500"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 20 16"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                      />
-                    </svg>
-                    {isDragActive ? (
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg
+                    className="w-8 h-8 mb-4 text-blue-500"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 16"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                    />
+                  </svg>
+                  {isDragActive ? (
+                    <p className="mb-2 p-4 sm:p-0 font-bold text-xl text-gray-500">
+                      <span>Drop the files here</span>
+                    </p>
+                  ) : (
+                    <>
                       <p className="mb-2 p-4 sm:p-0 font-bold text-xl text-gray-500">
-                        <span>Drop the files here</span>
+                        <span>Click to upload</span> or{" "}
+                        <span className="text-blue-500">Drag</span> and
+                        <span className="text-blue-500"> Drop</span>
                       </p>
-                    ) : (
-                      <>
-                        <p className="mb-2 p-4 sm:p-0 font-bold text-xl text-gray-500">
-                          <span>Click to upload</span> or{" "}
-                          <span className="text-blue-500">Drag</span> and
-                          <span className="text-blue-500"> Drop</span>
-                        </p>
-                        <p className="text-xs text-gray-500">
-                          SVG, PNG, JPG or GIF
-                        </p>
-                      </>
-                    )}
-                  </div>
-                  <input
-                    id="dropzone-file"
-                    className="hidden"
-                    {...getInputProps()}
-                  />
-                </label>
+                      <p className="text-xs text-gray-500">
+                        SVG, PNG, JPG or GIF
+                      </p>
+                    </>
+                  )}
+                </div>
+                <input {...getInputProps()} />
               </div>
 
-              <div className="flex xl:hidden mt-6 h-64 w-full flex-col items-center justify-center rounded-lg border-2 border-dashed border-blue-500 bg-blue-50 p-4 text-blue-500 transition-colors hover:border-blue-600  dark:border-blue-700 dark:bg-blue-900 dark:text-blue-400 dark:hover:border-blue-600 dark:hover:bg-blue-800">
+              <div className="flex xl:hidden mt-6 h-64 w-full flex-col items-center justify-center rounded-lg border-2 border-dashed border-blue-500 bg-blue-50 p-4 text-blue-500 transition-colors hover:border-blue-600 dark:border-blue-700 dark:bg-blue-900 dark:text-blue-400 dark:hover:border-blue-600 dark:hover:bg-blue-800 cursor-pointer">
                 <label
-                  htmlFor="dropzone-file"
+                  htmlFor="mobile-dropzone-file"
                   className="flex flex-col items-center justify-center w-full h-64 rounded-lg cursor-pointer bg-blue-50"
                 >
                   <div className="flex flex-col items-center justify-center pt-5 pb-6">
@@ -164,11 +155,11 @@ const DropBox = () => {
                     </p>
                   </div>
                   <input
-                    id="dropzone-file"
+                    id="mobile-dropzone-file"
                     type="file"
                     className="hidden"
-                    onChange={(e: React.ChangeEvent<any>) =>
-                      onFileSelect(e.target.files[0])
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      e.target.files && onFileSelect(e.target.files[0])
                     }
                   />
                 </label>
